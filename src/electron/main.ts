6e3960e5093c65113176545a385c44c29b69f9ec
Lloyd Boone <lboone@progressive.com>
2025-09-17 18:57:14 -0400

    feat: update app name to SysPulse and implement resource polling with IPC communication

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,12 +1,23 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
-import { isDev } from "./util.js";
+import { getPreloadPath } from "./pathResolver.js";
+import { getStaticData, pollResources } from "./resourceManager.js";
+import { ipcMainHandle, isDev } from "./util.js";
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+  });
   if (isDev()) {
     mainWindow.loadURL("http://localhost:5123/");
   } else {
     mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
   }
+
+  pollResources(mainWindow);
+  ipcMainHandle("getStaticData", () => {
+    return getStaticData();
+  });
 });
